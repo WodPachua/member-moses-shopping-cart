c7893ac2e8e4afe6ea2a3f89abc33f32b4070b86
wodpachua <molarag21996@gmail.com>
2024-12-18 01:27:50 +0300

    link logo to home page

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -11,10 +11,12 @@ const NavBar: React.FC<NavBarProps> = ({ toggleCart }) => {
 
   return (
     <header className="fixed top-0 left-0 w-full z-50 px-16 py-2 flex justify-between items-center bg-[#FFF6DC] shadow-md">
+      <Link to='/'>
       <div className='flex items-center'>
         <img src={Logo} alt="Logo" className='transform -translate-y-1 cursor-pointer' width='70px'/>
         <h1 className='text-2xl text-[#3D5B59] font-semibold ml-2 font-size[1.5rem]'>GoodNewsShop</h1>
       </div>
+      </Link>
       <nav className='flex gap-8'>
         <NavLink to='/' className={({ isActive }) => (isActive ? 'text-lg text-[#3D5B59] font-semibold relative after:content-[""] after:absolute after:bg-orange-500 after:h-[2px] after:w-full after:left-0 after:bottom-0' : 'text-lg text-gray-700 relative after:content-[""] after:absolute after:bg-orange-500 after:h-[2px] after:w-0 after:left-0 after:bottom-0 hover:after:w-full transition-all duration-300')} >
           Home
